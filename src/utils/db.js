4a328a47bb1e3994861a79ba114f5e6f4c8c3d55
Ubeyt <190541075@firat.edu.tr>
2021-09-28 12:25:18 +0300

    adds conditional ssl config

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,22 +1,27 @@
+// package required to connect postgresql
 
- // package required to connect postgresql
-
- import pg from "pg"
+import pg from "pg";
 
 // connection pool for postgresql
 
-const {DATABASE_URL:connectionString,NODE_ENV} = process.env;
+const { DATABASE_URL, DATABASE_URL_DEV, NODE_ENV } = process.env;
 
-const isProduction = NODE_ENV==='production';
+const isProduction = NODE_ENV === "production";
 
-console.log({isProduction})
+const connectionString = isProduction ? DATABASE_URL : DATABASE_URL_DEV;
 
+const sslConfig = isProduction
+  ? {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {};
+
+  
 const pool = new pg.Pool({
   connectionString,
-  ssl:{
-    rejectUnauthorized:false
-  }
-})
+  ...sslConfig,
+});
 
 export default pool;
- 
